32765de634bfa711a96a9f3ed3d32d5e21585d5e
Paulmuia <paulmuia7511@gmail.com>
2023-11-02 17:48:33 +0300

    added several things

--- lib/widgets/app_constants.dart
+++ lib/widgets/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants{
   static const String APP_NAME = "RealEstate";
   static const int APP_VERSION =1;
   static String TOKEN="token";
-  static const String BASE_URL="http://10.0.81.249:8000/";
+  static const String BASE_URL="http://192.168.137.58:8000/";
   static const String FEATURED_URI="featured";
   static  const String PLOT_URI = "plots";
   
